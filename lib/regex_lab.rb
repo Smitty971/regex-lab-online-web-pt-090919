fd79bbe0b59bf803b2fb72ac35508330b2a69cbf
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-30 16:42:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,8 @@ text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-text.grep(/^\w{5}\s/)
+#text.grep(/^\w{5}\s/)
+text.scan(/\[a-zA-Z]{5}\/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
